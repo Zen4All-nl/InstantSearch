a4f646af2a3c49c6a27e0f428c4660af3d801124
Zen4All <info@zen4all.nl>
2019-11-14 10:44:54 +0100

    Housekeeping
    - proper code indentation
    - replace tabs with spaces
    - remove trailing spaces

--- 1_Installation_files/includes/templates/YOUR_TEMPLATE/jscript/jscript_instantSearch.js
+++ 1_Installation_files/includes/templates/YOUR_TEMPLATE/jscript/jscript_instantSearch.js
@@ -26,170 +26,170 @@ var inputboxCurrent;
 $(document).ready(function () {
 
 
-	//this will apply the instant search feature to all the search boxes
-    var inputBox = $('input[name="keyword"]');
+  //this will apply the instant search feature to all the search boxes
+  var inputBox = $('input[name="keyword"]');
 
-	//if you want to add instant search to only a specific box then comment out the var inputBox above
-	//and uncomment out the specific search box selector bellow:
-	
-	//var inputBox = $('#navMainSearch > form[name="quick_find_header"] > input[name="keyword"]');
-    //var inputBox = $('#navColumnTwoWrapper > form[name="quick_find_header"] > input[name="keyword"]');
-	//var inputBox = $('#searchContent > form[name="quick_find"] > input[name="keyword"]');
-	
-	
-	//this adds a instant search container bellow the search box
-	inputBox.before('<div class="resultsContainer"></div>');
-	inputBox.attr('autocomplete', 'off');
-	
-	//re-position all the instant search container correctly into their places
-	if (autoPosition == true){
-		inputBox.each(function (index) {
-			var offset = $(this).offset();
-			$(this).prev().css("left", offset.left + "px");
-			$(this).prev().css("top", ($(this).outerHeight(true) + offset.top) + "px");
-		});
-	}
+  //if you want to add instant search to only a specific box then comment out the var inputBox above
+  //and uncomment out the specific search box selector bellow:
+
+  //var inputBox = $('#navMainSearch > form[name="quick_find_header"] > input[name="keyword"]');
+  //var inputBox = $('#navColumnTwoWrapper > form[name="quick_find_header"] > input[name="keyword"]');
+  //var inputBox = $('#searchContent > form[name="quick_find"] > input[name="keyword"]');
 
 
-	//if the search box losses focus, then the instant search container will be hidden
-    inputBox.blur(function () {
-        if (inputboxCurrent) {
-            var resultsContainer = inputboxCurrent.prev();
-            resultsContainer.delay(300).slideUp(200);
-        }
+  //this adds a instant search container bellow the search box
+  inputBox.before('<div class="resultsContainer"></div>');
+  inputBox.attr('autocomplete', 'off');
+
+  //re-position all the instant search container correctly into their places
+  if (autoPosition == true) {
+    inputBox.each(function (index) {
+      var offset = $(this).offset();
+      $(this).prev().css("left", offset.left + "px");
+      $(this).prev().css("top", ($(this).outerHeight(true) + offset.top) + "px");
     });
+  }
 
 
-	//if we resize the browser or zoom in or out of a page then the instant search container will be hidden
-	$(window).resize(function() {
-        if (inputboxCurrent) {
-            var resultsContainer = inputboxCurrent.prev();
-            resultsContainer.hide();
-        }
-	});
+  //if the search box losses focus, then the instant search container will be hidden
+  inputBox.blur(function () {
+    if (inputboxCurrent) {
+      var resultsContainer = inputboxCurrent.prev();
+      resultsContainer.delay(300).slideUp(200);
+    }
+  });
 
 
-	//the user starts to enter a few characters into the search box
-    inputBox.keyup(function () {
+  //if we resize the browser or zoom in or out of a page then the instant search container will be hidden
+  $(window).resize(function () {
+    if (inputboxCurrent) {
+      var resultsContainer = inputboxCurrent.prev();
+      resultsContainer.hide();
+    }
+  });
 
-		//only the currently selected search box will be used
-        inputboxCurrent = $(this);
 
-		//assign a variable to the instant search container
-        var resultsContainer = $(this).prev();
+  //the user starts to enter a few characters into the search box
+  inputBox.keyup(function () {
 
-        //we capture the words that are being typed into the search box
-        var searchWord = $(this).val();
-		var replaceWord = searchWord;
-		
-		//we clean up the word for any unnecessary characters or double spaces
-        searchWord = searchWord.replace(/^\s+/, "");
-        searchWord = searchWord.replace(/  +/g, ' ');
+    //only the currently selected search box will be used
+    inputboxCurrent = $(this);
 
-       
-        if (searchWord == "") {
+    //assign a variable to the instant search container
+    var resultsContainer = $(this).prev();
 
-            //if the search value entered is empty, we then hide the instant search container	
-            resultsContainer.hide();
+    //we capture the words that are being typed into the search box
+    var searchWord = $(this).val();
+    var replaceWord = searchWord;
 
-        } else {
+    //we clean up the word for any unnecessary characters or double spaces
+    searchWord = searchWord.replace(/^\s+/, "");
+    searchWord = searchWord.replace(/  +/g, ' ');
 
-            //if multiple requests are sent to the server, we then abort any previous request, before a new request is sent
-			//this only comes in use if user is a fast typer
-            if (runningRequest) {
-                request.abort();
+
+    if (searchWord == "") {
+
+      //if the search value entered is empty, we then hide the instant search container	
+      resultsContainer.hide();
+
+    } else {
+
+      //if multiple requests are sent to the server, we then abort any previous request, before a new request is sent
+      //this only comes in use if user is a fast typer
+      if (runningRequest) {
+        request.abort();
+      }
+
+      runningRequest = true;
+
+      //we then pass on the search word to searches.php
+      //searches.php will then look for all the search results 
+      request = $.getJSON('searches.php', {query: searchWord}, function (data) {
+
+
+        if (data.length > 0) {
+          var resultHtml = '';
+          $.each(data, function (i, item) {
+            //if any search result are found, a link will be created and placed into the instant search container
+            resultHtml += '<li><a href="' + generateLink(item.pc, item.l, item.pt, item.q) + '"><span class="alignRight">' + formatNumber(item.c) + '</span>' + highlightWord(replaceWord, item.q) + '</a></li>';
+          });
+
+          //added for the more link at the bottom uncomment if like it
+          // resultHtml += '<li><a href="index.php?main_page=advanced_search_result&search_in_description=1&keyword=' + replaceWord + '"><span class="alignRight searchMore">More result...</span></a></li>';
+
+          //fill the container with the matching products and categories
+          resultsContainer.html('<ul>' + resultHtml + '</ul>');
+
+          if (!resultsContainer.is(':visible')) {
+
+            //auto position container if needs be
+            if (autoPosition == true) {
+              autoPositionContainer(inputboxCurrent, resultsContainer);
             }
 
-            runningRequest = true;
-           
-			//we then pass on the search word to searches.php
-			//searches.php will then look for all the search results 
-            request = $.getJSON('searches.php', {query: searchWord}, function (data) {
-                
-				
-                if (data.length > 0) {
-                    var resultHtml = '';
-                    $.each(data, function (i, item) {
-						//if any search result are found, a link will be created and placed into the instant search container
-                        resultHtml += '<li><a href="' + generateLink(item.pc,item.l,item.pt,item.q) + '"><span class="alignRight">' + formatNumber(item.c) + '</span>' + highlightWord(replaceWord,item.q) + '</a></li>';
-                    });
-                      
-                       //added for the more link at the bottom uncomment if like it
-			// resultHtml += '<li><a href="index.php?main_page=advanced_search_result&search_in_description=1&keyword=' + replaceWord + '"><span class="alignRight searchMore">More result...</span></a></li>';
-			
-					//fill the container with the matching products and categories
-                    resultsContainer.html('<ul>'+resultHtml+'</ul>');
-					
-                    if (!resultsContainer.is(':visible')) {
-						
-						//auto position container if needs be
-						if (autoPosition == true){
-							autoPositionContainer(inputboxCurrent, resultsContainer);
-						}
-						
-						//drop down instant search box
-                        resultsContainer.slideDown(200);
-                    }
-					
-                } else {
-                    resultsContainer.hide();
+            //drop down instant search box
+            resultsContainer.slideDown(200);
+          }
 
-                }
+        } else {
+          resultsContainer.hide();
 
-                runningRequest = false;
-            });
         }
-    });
+
+        runningRequest = false;
+      });
+    }
+  });
 });
 
 //this function auto positions the container
-function autoPositionContainer(inputBoxCurr, resltsContainer){
-	var offsetInput = inputBoxCurr.offset();
-	var overFlow = offsetInput.left + resltsContainer.outerWidth(true);
-	var winWidth = $(document).width();
-	
-	if (overFlow > winWidth){ // this checks to see if the container overflows on the right of the window
-		var dif = overFlow - winWidth;
-		
-		if ((offsetInput.left - dif) < 0){// this checks to see if the container overflows on the left of the window
-			resltsContainer.css("left", 0 + "px");
-		}else{
-			resltsContainer.css("left", (offsetInput.left - dif) + "px");
-		}
-	}else{
-		resltsContainer.css("left", offsetInput.left + "px");
-	}
-	resltsContainer.css("top", (inputBoxCurr.outerHeight(true) + offsetInput.top) + "px");
+function autoPositionContainer(inputBoxCurr, resltsContainer) {
+  var offsetInput = inputBoxCurr.offset();
+  var overFlow = offsetInput.left + resltsContainer.outerWidth(true);
+  var winWidth = $(document).width();
+
+  if (overFlow > winWidth) { // this checks to see if the container overflows on the right of the window
+    var dif = overFlow - winWidth;
+
+    if ((offsetInput.left - dif) < 0) {// this checks to see if the container overflows on the left of the window
+      resltsContainer.css("left", 0 + "px");
+    } else {
+      resltsContainer.css("left", (offsetInput.left - dif) + "px");
+    }
+  } else {
+    resltsContainer.css("left", offsetInput.left + "px");
+  }
+  resltsContainer.css("top", (inputBoxCurr.outerHeight(true) + offsetInput.top) + "px");
 }
 
 //this function creates the link back to the matching products or categories 
 function generateLink(productORcategory, productCategoryID, productInfopage, productKeyword)
 {
-	var l = "";
-	    
-	if (productORcategory == "p"){
-		l = "index.php?main_page=" + productInfopage + "&products_id=" + productCategoryID;
-	}else if (productORcategory == "c"){
-		l = "index.php?main_page=index&cPath=" + productCategoryID;
-	}else {
-	        l = "index.php?main_page=advanced_search_result&keyword=" + productKeyword + "&manufacturers_id=" + productCategoryID;
-	}
-	return l;
+  var l = "";
+
+  if (productORcategory == "p") {
+    l = "index.php?main_page=" + productInfopage + "&products_id=" + productCategoryID;
+  } else if (productORcategory == "c") {
+    l = "index.php?main_page=index&cPath=" + productCategoryID;
+  } else {
+    l = "index.php?main_page=advanced_search_result&keyword=" + productKeyword + "&manufacturers_id=" + productCategoryID;
+  }
+  return l;
 
 }
 
 
-function highlightWord(findTxt,replaceTxt)
+function highlightWord(findTxt, replaceTxt)
 {
-	var f = findTxt.toLowerCase();
-	var r = replaceTxt.toLowerCase();
-	var regex = new RegExp('(' + f + ')', 'i');
-	return r.replace(regex, '<span class="thinFont">' + f + '</span>')
-	
+  var f = findTxt.toLowerCase();
+  var r = replaceTxt.toLowerCase();
+  var regex = new RegExp('(' + f + ')', 'i');
+  return r.replace(regex, '<span class="thinFont">' + f + '</span>')
+
 }
 
 function formatNumber(num)
 {
-	return num.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-	
+  return num.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+
 }
